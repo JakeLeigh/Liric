e83349da023cbb5621cc6b777fb22d8dbe2ce297
Jake Leigh <jake@jakeleigh.io>
2021-01-10 21:12:25 +0000

    add searchbar lib

--- Liric/App.js
+++ Liric/App.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SearchBar} from 'react-native-elements';
+import {SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+function App() {
+  const [lyric, setLyric] = useState('');
+  useEffect(() => {
+    SplashScreen.hide();
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+  }, []);
   return (
     <View>
-      <Text style={styles.testTitle}>Litic</Text>
+      <StatusBar backgroundColor="#203059" barStyle="default" />
+      <Text style={styles.testTitle}>Liric</Text>
+      <SearchBar
+        placeholder='Enter Song Lyric'
+        onChangeText={(e) => setLyric(e)}
+        value={lyric}
+      />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   testTitle: {
     textAlign: 'center',
-    fontSize: 18,
+    fontSize: 38,
     marginTop: 65,
   },
 });
